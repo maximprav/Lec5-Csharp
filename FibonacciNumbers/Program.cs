e185d1ed137271f124c361ac7697c699d46b932a
Maxim Prav <mxmlknv@mail.ru>
2023-02-25 19:31:52 +0300

    Числа Фибоначчи

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+Console.Clear();
+for (int i = 1; i < 51; i++)
+{
+    Console.WriteLine($"f({i}) - {Fibonacci(i)}");
+}
